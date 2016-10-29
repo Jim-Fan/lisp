686c07a12205ca00591aba2ed3c9abc2b8d6e5de
Jim Fan <iamjimfan@gmail.com>
2016-10-29 21:16:13 +0800

    Add symlist cleanup and get, bug fix

--- src/symlist.h
+++ src/symlist.h
@@ -1,21 +1,68 @@
 #ifndef _LISP_SYMLIST_
 #define _LISP_SYMLIST_
 
+#include <stdio.h>
 #include "cell.h"
 
 cell* ENV = NULL;
 
 void symlist_init();
-//void symlist_cleanup();
-//cell* symlist_get(char*);
+void symlist_cleanup();
+cell* symlist_get(char*);
 //cell* symlist_put(char*, cell*);
 
 ////////////////////////////////////////////////////////////
 
 
+cell* symlist_get(char* sym)
+{
+  if (ENV == NULL) return NULL;
+
+  cell* get = ENV;
+  while (get != NULL)
+  {
+    if (strcmp(sym, (char*)get->car->car) == 0) {
+      return get->car->cdr;
+    }
+    get = get->cdr;
+  }
+
+  // not found
+  return NULL;
+}
+
+void symlist_cleanup()
+{
+  if (ENV == NULL) return;
+
+  cell* del = ENV;
+  int d = 0;
+  while (del != NULL)
+  {
+    ENV = del->cdr;
+
+    // del->car is symbol name-val entry
+
+    // name
+    free((char*) del->car->car);
+
+    // val
+    free_cell((cell*) del->car->cdr);
+
+    free(del);
+    ++d;
+    del = ENV;
+  }
+
+  if (ENV != NULL) {
+    printf("symlist_cleanup: ENV is not null\n");
+  }
+  printf("symlist_cleanup: %d backbone nodes deleted\n", d);
+}
+
 void symlist_init()
 {
-  char* c = "a";
+  char c[] = "a";
   int i = 1;
 
   cell* backbone = NULL;
@@ -38,7 +85,7 @@ void symlist_init()
 
   // symbol name becomes "b";
 
-  while (c[0] < 'z')
+  while (c[0] <= 'z')
   {
     // ENV and backbone point to 1st node
 
